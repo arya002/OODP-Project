65dc9cd67adab1e61cb225fcb9dcc27a2ff2563d
slageveen <steven.lageveen@student.uva.nl>
2019-11-03 17:05:54 +0800

    Added print method and more ways to edit seats

--- src/RoomLayout.java
+++ src/RoomLayout.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RoomLayout {
     private String[][] seats;
+    private HashMap<String, Character> representations
 
     public RoomLayout(String file) {
 //        TODO read matrix from file.
@@ -7,10 +12,42 @@ public class RoomLayout {
 
     public RoomLayout(int n, int m) {
         this.seats = new String[n][m];
+        for (String[] row : this.seats) {
+            Arrays.fill(row, "None")
+        }
+        this.representations = new HashMap<String, Character>();
+        representations.put("None", ' ');
+        representations.put("Normal", 'N');
+        representations.put("Premium", 'P');
+        representations.put("Love", 'L');
+
     }
 
     public void assignType(int n, int m, String type) {
         this.seats[n][m] = type;
+    }
+
+    public void assignType(int pos, String type) {
+        this.seats[pos/this.seats[0].length][pos%this.seats[0].length] = type;
+    }
+
+    public void assignTypes(HashMap<Integer, String> locations) {
+        for (Map.Entry<Integer, String> entry : locations.entrySet()) {
+            this.assignType(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public void print() {
+        String line = "-".repeat(this.seats[0].length);
+        System.out.println(" " + line);
+        for (String[] row : this.seats) {
+            System.out.print("|");
+            for (String seat : row) {
+                System.out.print(this.representations.get(seat));
+            }
+            System.out.println("|");
+        }
+        System.out.println(" " + line);
     }
 
     public void save() {
