1edda058037a650470a3dcca230605d54c928e4e
changchuanhong <cchong123@hotmail.com>
2019-11-05 15:07:16 +0800

    Update Review.java

--- src/Review.java
+++ src/Review.java
@@ -2,10 +2,21 @@ public class Review {
     private String review;
     private double rating;
 
-    public void print() {
+    public void print() { // print review
+        System.out.println(rating);
+        System.out.println(review);
     }
 
     public double getRating() {
         return rating;
+    }
+    
+    public String getReview(){
+        return review;
+    }
+    
+    public void setReview(String review, double rating) {
+        this.review = review;
+        this.rating = rating;
     }
 }
