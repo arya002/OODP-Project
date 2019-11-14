60e88a2d4b267a53d6e80755d468add2b81d2e1c
arya002 <54712381+arya002@users.noreply.github.com>
2019-11-14 16:44:58 +0800

    Update LoginScreen.java

--- src/LoginScreen.java
+++ src/LoginScreen.java
@@ -6,18 +6,54 @@ public class LoginScreen {
 
 	private String password = "";
 	private String userName;
+	private String type;
 	private int tries;
+	private boolean loggedIn;
 
 	public LoginScreen(){
 		this.password = "guest";
 		this.userName = "guest";
+		this.type = "";
 		this.tries = 3;
+		this.loggedIn = false;
+		run();
 	}
+
+	public void run() {
+        int sc_in;
+
+        do {
+            System.out.println("1. Login \n2. Continue as Guest \n3. Client Register\n4. Exit");
+            sc_in = sc.nextInt();
+            switch (sc_in) {
+                case 1:
+                    enterUserName();
+                    enterPassword();
+                    break;
+                case 2:
+//					TODO make guest
+                    break;
+                case 3:
+                    registerUser();
+                    loggedIn = true;
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Invalid input, please choose from the following:");
+                    break;
+            }
+
+        } while (sc_in != 4 && sc_in != 2 && !loggedIn);
+
+    }
 
 	public void enterUserName(){
 
 		System.out.print("Enter username : ");
 		this.userName = sc.next();
+		//check if invalid username and throw error
+        //also get type of user
 	}
 
 	public void enterPassword(){
@@ -25,6 +61,12 @@ public class LoginScreen {
 		if(tries > 0) {
 			System.out.print("Enter password ( " + tries + " left ) : ");
 			this.password = sc.next();
+			    //check value of password from file
+			if(password.equals()) {
+                loggedIn = true;
+                System.out.println("Successfully logged in!");
+                return;
+            }
 			tries -= 1;
 		}
 		else{
@@ -42,19 +84,12 @@ public class LoginScreen {
 		return this.password;
 	}
 
-	public String getType(){
+	public void registerUser() {
+	    System.out.println("Enter a username:");
+	    userName = sc.next();
+	        //check if username already exists and throw error
+        password = sc.next();
 
-		// check if userName is in the list of staff usernames
-		if(true){
-			return "staff";
-		}
-		// check if userName is in the list of customer usernames
-		else if(true){
-			return "staff";
-		}
-		//guest login if any other username
-		else{
-			return "guest";
-		}
-	}
+    }
+
 }
