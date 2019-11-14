17ae31a4c587e7c345be072bec568e1fe18e68d3
Beck Gillespie <beckjgillespie@gmail.com>
2019-11-14 16:49:17 +0800

    Update Data.java

--- src/Data.java
+++ src/Data.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Data implements Serializable {
+
     static private ArrayList<Cineplex> locations;
     static private ArrayList<Client> clients;
     static private ArrayList<Staff> staff;
@@ -13,21 +14,6 @@ public class Data implements Serializable {
         this.locations = new ArrayList<>();
         this.clients = new ArrayList<>();
         this.staff = new ArrayList<>();
-    }
-
-    static public Data getInstance() {
-        System.out.println("Gathering data please hold...");
-        if (instance == null) {
-            instance = (Data) loadObject(path);
-            if (instance == null)
-                instance = new Data();
-        }
-        System.out.println("Finished thank you for waiting...\n\n");
-        return instance;
-    }
-
-    public void save() {
-        saveObject(instance, path);
     }
 
     //<editor-fold desc="Adders">
@@ -66,37 +52,84 @@ public class Data implements Serializable {
     }
     //</editor-fold>
 
-    static private Object loadObject(String file) {
-        try {
-            FileInputStream f = new FileInputStream(file);
-            ObjectInputStream in = new ObjectInputStream(f);
-            Object temp =  in.readObject();
-            in.close();
-            f.close();
-            return temp;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            return null;
+    static public Data getInstance() {
+        System.out.println("Gathering data please hold...");
+        if (instance == null) {
+            try {
+                //instance = (Data) loadObject(path);
+                if (instance == null)
+                    instance = new Data();
+            }catch (Exception e){
+
+            }
+        }
+        System.out.println("Finished thank you for waiting...\n\n");
+        return instance;
+    }
+
+    public void saveObjectToPath(String path,ArrayList<?> arrayToSave) {
+        if (arrayToSave.size() !=0) {
+            SaveLoad saveLoad = new SaveLoad(arrayToSave.get(0).getClass());
+            saveLoad.saveObject(arrayToSave,path);
+        } else {
+            System.out.println("Array empty :(");
         }
     }
 
-    static private void saveObject(Object o, String file) {
-        try {
-            FileOutputStream f = new FileOutputStream(file, false);
-            ObjectOutputStream out = new ObjectOutputStream(f);
-            out.writeObject(o);
-            out.close();
-            f.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public ArrayList<?> getObjectFromPath(String path,Class o){
+        System.out.println(o);
+        SaveLoad pair= new SaveLoad<>(o);
+        ArrayList objectsFromPath = pair.loadObject(path);
+        System.out.println(objectsFromPath.size() + " size of array");
+        return objectsFromPath;
+
+    }
+
+
+    public class SaveLoad<K> {
+
+        private K key;
+
+        public SaveLoad(K key) {
+            this.key = key;
+        }
+
+        public ArrayList<K> loadObject(String file) {
+            try {
+                System.out.println(System.getProperty("user.dir"));
+                FileInputStream f = new FileInputStream(file);
+                ObjectInputStream in = new ObjectInputStream(f);
+                ArrayList<K> retArray;
+                retArray = (ArrayList<K>) in.readObject();
+                System.out.println(retArray.size() + " size of array");
+                in.close();
+                f.close();
+                return retArray;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        public void saveObject(ArrayList<K> o, String file) {
+
+            try {
+                FileOutputStream f = new FileOutputStream(file);
+                ObjectOutputStream out = new ObjectOutputStream(f);
+                out.writeObject(o);
+                out.close();
+                f.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
