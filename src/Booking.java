68eea4b57761ba4b46c954e4586b4c256f684691
changchuanhong <cchong123@hotmail.com>
2019-11-18 12:07:35 +0800

    Update Booking.java

--- src/Booking.java
+++ src/Booking.java
@@ -2,7 +2,9 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-
+/**
+Is a booking object which holds the tickets, client, price, showing slot and identifying ID of a booking.
+ */
 public class Booking implements Serializable{
 
     private ArrayList<Ticket> tickets;
@@ -11,14 +13,14 @@ public class Booking implements Serializable{
     private Showing showing;
     private String bookingID;
 
-    public Booking(ArrayList<Ticket> tickets, Client client, Showing showing) {
-/**
- * Create a new booking
+    /**
+ * Create a new booking object
  *@param tickets list of tickets selected
   *@param client identity of client
    *@param showing particular timeslot of showing
    *It then timestamps and get the total price of the tickets for the customer.
  */
+    public Booking(ArrayList<Ticket> tickets, Client client, Showing showing) {
         this.tickets = tickets;
         this.client = client;
         this.showing = showing;
@@ -33,28 +35,36 @@ public class Booking implements Serializable{
         this.bookingID = showing.getCineplex().getName().substring(0, 3).toUpperCase() + format1.format(calendar.getTime());
     }
 /**
- * Get Methods
+ * Gets the total price of the tickets
+ @return the price (double)
  */
     public double getTotalPrice()
     {
         return totalPrice;
     }
-
+/**
+ * Gets the id of booking
+ @return the id as a string
+ */
     public String getBookingID()
     {
         return bookingID;
     }
-
+/**
+ * Gets the client who made the booking
+ @return client object
+ */
     public Client getClient() {
         return client;
     }
-
+/**
+ * Gets the arraylist of tickets booked
+ @return the tickets as an array list
+ */
     public ArrayList<Ticket> getTickets()
     {
         return tickets;
     }
-
-    public String bookingPrint(){
         /**
  * Print the booking details in to format:
  * Booking ID
@@ -62,6 +72,7 @@ public class Booking implements Serializable{
  *Movie
  *Number of Tickets
  */
+    public String bookingPrint(){
         String retString="";
         retString+= "Booking ID" + bookingID + "\n";
         retString+= "Customer " + client.getFirstName() + "\n";
