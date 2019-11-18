20e650b5b130e0c9ad5279b54864da29272188be
Beck Gillespie <beckjgillespie@gmail.com>
2019-11-18 23:00:13 +0800

    login register now works

--- src/ClientApp.java
+++ src/ClientApp.java
@@ -172,8 +172,7 @@ public class ClientApp {
                 for (Movie movie : MovieControl.getAllMovies()) {
                     if (mov.equals("exit"))
                         break;
-                    System.out.println("Looking for " + mov);
-                    System.out.println(movie.getName());
+
                     if (movie.getName().contains(mov)) {
 
                         System.out.println("Movie Found");
