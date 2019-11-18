b7f6d9b4ebc0ff22544c6a45ab841367a426575a
ajivila <livijadana@gmail.com>
2019-11-18 10:43:14 +0800

    reviews

--- src/Review.java
+++ src/Review.java
@@ -27,13 +27,6 @@ public class Review implements Serializable {
 
     }
 
-    public void print() { // print review
-        System.out.println("Review by: " + this.reviewer.getUsername());
-        System.out.println("Movie: " + this.movieName);
-        System.out.println("Date: " + this.date);
-        //System.out.printf("*".repeat((int) rating) + ", %.1f/5\n\n", rating);
-        System.out.println("Review:\n" + review);
-    }
 
     //<editor-fold desc="Getters">
     public double getRating() {
