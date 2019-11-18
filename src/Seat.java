995f87ca5824da8e2249ccb596f45944775f43d4
arya002 <54712381+arya002@users.noreply.github.com>
2019-11-19 00:08:20 +0800

    Update Seat.java

--- src/Seat.java
+++ src/Seat.java
@@ -1,28 +1,62 @@
 import java.io.Serializable;
 import java.util.HashMap;
 
+/**
+ * Represents a seat in the cinema
+ */
 public class Seat implements Serializable{
 
+    /**
+     * Seat ID
+     */
     private String seatID;
+
+    /**
+     * Customer ID who booked the seat
+     */
     private String custID;
+
+    /**
+     * Type of the seatt
+     */
     private String type;
+
+    /**
+     * Specifies whether seat is allocated or not
+     */
     private boolean allocated;
 
+    /**
+     * Creates a new seat in the cinema
+     * @param seatID Seat ID
+     * @param type Seat type
+     */
     public Seat(String seatID, String type) throws IllegalArgumentException {
         this.setSeatID(seatID);
         this.setType(type);
         this.allocated = false;
     }
 
-    //<editor-fold desc="Getters">
+    /**
+     * Gets the seat ID
+     * @return seat ID
+     */
     public String getSeatID() {
         return seatID;
     }
 
+    /**
+     * Gets Customer ID
+     * @return Customer ID
+     */
     public String getCustID() {
         return custID;
     }
 
+    /**
+     * Gets seat type
+     * @return Seat type
+     */
     public String getType() {
         return type;
     }
@@ -30,33 +64,50 @@ public class Seat implements Serializable{
     public boolean isAllocated() {
         return allocated;
     }
-    //</editor-fold>
 
-    //<editor-fold desc="Setters">
+    /**
+     * Changes the seat ID
+     */
     public void setSeatID(String seatID) {
         this.seatID = seatID;
     }
 
+    /**
+     * Changes the customer ID
+     */
     public void setCustID(String custID) {
         this.custID = custID;
     }
 
+    /**
+     * Changes the seat type
+     * @param type Seat type
+     */
     public void setType(String type) {
         if (type.isEmpty())
             throw new IllegalArgumentException("Type cannot be empty");
         this.type = type;
     }
 
+    /**
+     * Assigns a seat to a customer
+     * @param custID Customer ID
+     */
     public void assignSeat(String custID) {
         this.allocated = true;
         this.setCustID(custID);
     }
 
+    /**
+     * Unassigns a seat
+     */
     public void unassignSeat() {
         this.allocated = false;
     }
-    //</editor-fold>
 
+    /**
+     * Prints seat information
+     */
     public void print() {
         System.out.println("seatID: " + seatID);
         System.out.println("custID: " + custID);
